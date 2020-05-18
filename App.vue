42f9e623005ad73f877a165abc8cee69de232354
김연욱 / YeonUk Kim <yeonuk44@gmail.com>
2020-05-18 00:30:04 -0700

    Create AppBar

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+
+    <v-content>
+      <AppBar/>
+      <HelloWorld/>
+      <MainPage/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+import AppBar from './components/AppBar';
+import MainPage
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    AppBar,
+
+  },
+
+  data: () => ({
+    
+  }),
+};
+</script>
---
+++ components/AppBar.vue
@@ -0,0 +1,41 @@
+<script>
+  export default {
+    name: 'AppBar',
+    data:()=>({
+        justify: [
+            'start',
+            'end',
+            'center',
+            'space-between',
+            'space-around',
+        ]
+    })
+  }
+</script>
+
+<template>
+  <div>
+    <v-app-bar
+      color="#000000"
+      dense
+      dark
+      height="70em"
+
+
+    >
+      
+      <v-app-bar-nav-icon
+      width="46 em">
+          <v-icon >
+          </v-icon>
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title> 
+          <p class="display-1">
+          Studec</p></v-toolbar-title>
+
+
+      
+    </v-app-bar>
+  </div>
+</template>
